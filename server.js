8ec64fe6d301a5da22a95098cedbdad76c8c1ff5
jedediahriley <46664082+jedediahriley@users.noreply.github.com>
2021-04-14 20:08:48 -0400

    created server

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-const express = require('express')
+// Dependencies
+const express = require('express');
+const APP = express();
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const DB = mongoose.connection
+require('dotenv').config()
+
+// Port
+const PORT = process.env.PORT || 3000;
+
+// Database Connection
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/notes';
+
+// Connect to Mongo
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true });
+
+// Opening the connection to mongo
+DB.on('open' , ()=>{});
+
+// middleware
+APP.use(express.urlencoded({extended: true}))
+APP.use(methodOverride('_method'))
+//APP.get('/', (req, res) => {res.redirect('/notes')})
+//APP.use('/equipment', equipController)
+APP.use(express.static('public'));
+
+
+
+
+APP.listen(PORT, () => {
+    console.log('hello')
+})
